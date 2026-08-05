65c1bd02c9b16bcbd7eb6b23bf7ea8785c16a1ee
重霖 黄 <3564567691@qq.com>
2026-08-05 10:27:35 +0800

    修复学生管理器中的字典键错误，并完善添加和删除学生的功能

--- sc_management.py
+++ sc_management.py
@@ -8,8 +8,16 @@ class StudentManager:
             raise ValueError(f"学生{student_id}已存在")
         if not isinstance(age,int) or age<0 or age>150:
             raise ValueError(age,f"年龄{age},不在合法范围内(1~150)")
-        self.student[student_id]={
-            “name" : name,
+        self.students[student_id]={
+            "name" : name,
             "age" : age,
-            "score" : score or []
+            "score": score or []
         }
+        print(f"√ 添加学生{name}c成功")
+
+        def remove_student(self, student_id):
+            if student_id not in self.students:
+                raise ValueError(f"学生{student_id}不存在")
+            name = self.students[student_id]["name"]
+            del self.students[student_id]
+            print(f"√ 删除学生{name}成功")
